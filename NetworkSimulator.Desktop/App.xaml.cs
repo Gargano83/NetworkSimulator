6fbb0c21c7440ff2a253cc73e5dd0f913df42b30
a.iannetti <a.iannetti@si-t.it>
2025-09-26 19:53:25 +0200

    Modifica per apertura app desktop in modalità "fullscreen"

--- NetworkSimulator.Desktop/App.xaml.cs
+++ NetworkSimulator.Desktop/App.xaml.cs
@@ -1,15 +1,46 @@
-﻿namespace NetworkSimulator.Desktop
+﻿using Microsoft.UI.Windowing;
+
+namespace NetworkSimulator.Desktop
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            MainPage = new MainPage();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "NetworkSimulator.Desktop" };
+            var window = base.CreateWindow(activationState);
+
+            // Invece di 'Created', usiamo 'Activated' per essere sicuri che la finestra sia pronta
+            window.Activated += OnWindowActivated;
+
+            return window;
+        }
+
+        private void OnWindowActivated(object sender, EventArgs e)
+        {
+            var window = sender as Window;
+            if (window == null) return;
+
+            // Annulla l'iscrizione all'evento per assicurarti che questo codice venga eseguito UNA SOLA VOLTA
+            window.Activated -= OnWindowActivated;
+
+#if WINDOWS
+            var nativeWindow = window.Handler.PlatformView as Microsoft.UI.Xaml.Window;
+            if (nativeWindow != null)
+            {
+                var appWindow = nativeWindow.AppWindow;
+                var presenter = appWindow.Presenter as OverlappedPresenter;
+                if (presenter != null)
+                {
+                    presenter.Maximize();
+                }
+            }
+#endif
         }
     }
 }
